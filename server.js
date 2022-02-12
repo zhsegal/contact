2d8473582023502e6f3a8a1df1bec382609a75f2
zvika <zhsegal@gmail.com>
2022-02-12 14:06:14 +0200

    'gfd'

--- server.js
+++ server.js
@@ -13,22 +13,38 @@ const mongoURI = "mongodb+srv://admin-zvika:5293612aA!@cluster0.w9hbu.mongodb.ne
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const patientSchema = new mongoose.Schema({
-ID:String
+ID:String,
+GENDER: String,
+AGE: String,
 
+TIME: String
 })
 const Patient = mongoose.model("Patient", patientSchema)
 
 
 app.post('/', (req, res) => {
-
+var year=new Date().getDate().toString()
+var day=new Date().getFullYear().toString()
+var month=new Date().getMonth()
+var monthi= month + 1
+var time= year +'-'+ monthi + '-' + day
 
 var patient=new Patient({
-ID:req.body.ID
+ID:req.body.ID,
+GENDER:req.body.GENDER,
+AGE: req.body.AGE,
+
+
+
+
+
+TIME: time
 })
 
 patient.save()
-res.send(req.body)
+
 console.log(patient)
+res.redirect('/')
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
